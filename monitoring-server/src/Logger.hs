e0428335839ff5a7d6f9841ea1ade51d927463d7
Woodson Delhia <woodsondelhia88@gmail.com>
2019-06-29 16:13:45 +0900

    Added MonadLogger instance for Katip and ServerAppT

--- monitoring-server/src/Logger.hs
+++ monitoring-server/src/Logger.hs
@@ -1,13 +1,34 @@
 module Logger where
 
-import           Katip     (ColorStrategy (..), LogEnv, Severity (..),
-                            Verbosity (..), defaultScribeSettings, initLogEnv,
-                            mkHandleScribe, registerScribe)
+import qualified Control.Monad.Logger  as Logger
+import           Katip                 (ColorStrategy (..), LogEnv, LogStr,
+                                        Namespace (..), Severity (..),
+                                        Verbosity (..), defaultScribeSettings,
+                                        initLogEnv, logStr, mkHandleScribe,
+                                        registerScribe)
 import           RIO
-import qualified System.IO as IO
+import qualified System.IO             as IO
+import           System.Log.FastLogger as FastLogger
 
 defaultLogEnv :: IO LogEnv
 defaultLogEnv = do
-    handleScribe <- mkHandleScribe ColorIfTerminal IO.stdout DebugS V2
-    env <- initLogEnv "servant-persistent" "production"
-    registerScribe "stdout" handleScribe defaultScribeSettings env
+    handleScribe <- Katip.mkHandleScribe ColorIfTerminal IO.stdout DebugS V2
+    env <- Katip.initLogEnv "servant-persistent" "production"
+    Katip.registerScribe "stdout" handleScribe Katip.defaultScribeSettings env
+
+fromLevel :: Logger.LogLevel -> Severity
+fromLevel Logger.LevelDebug     = DebugS
+fromLevel Logger.LevelInfo      = InfoS
+fromLevel Logger.LevelWarn      = WarningS
+fromLevel Logger.LevelError     = ErrorS
+fromLevel (Logger.LevelOther _) = NoticeS
+
+-- | Transforms Katip logMsg into monadLoggerLog to be used inside
+-- MonadLogger monad
+liftKatip :: (ToLogStr msg)  =>
+         (Katip.Namespace -> Severity -> Katip.LogStr -> m ()) ->
+          Logger.Loc -> Logger.LogSource -> Logger.LogLevel -> msg -> m ()
+liftKatip f _ src lvl msg = f ns (fromLevel lvl) $ logStr' msg
+  where
+    ns = Namespace [src]
+    logStr' = Katip.logStr . FastLogger.fromLogStr . Logger.toLogStr
--- monitoring-server/src/ServerApp.hs
+++ monitoring-server/src/ServerApp.hs
@@ -1,18 +1,31 @@
-module ServerApp where 
+module ServerApp where
 
-import Control.Monad.Except (ExceptT, MonadError)
-import           Control.Monad.Metrics                (MonadMetrics, getMetrics)
-import RIO
-import Servant (ServantErr)
-import Config
+import           Config
+import           Control.Monad.Except  (ExceptT, MonadError)
+import           Control.Monad.Logger
+import           Control.Monad.Metrics (MonadMetrics, getMetrics)
+import           Katip
+import           Logger                (liftKatip)
+import           RIO
+import           Servant               (ServantErr)
 
--- ServerApp contains the monad that all Servant handlers will be running in. 
-newtype ServerAppT m a = 
-    ServerAppT 
-    { runServerApp :: ReaderT Config (ExceptT ServantErr m) a 
+-- ServerApp contains the monad that all Servant handlers will be running in.
+newtype ServerAppT m a =
+    ServerAppT
+    { runServerApp :: ReaderT Config (ExceptT ServantErr m) a
     } deriving (Functor, Applicative, Monad, MonadReader Config, MonadError ServantErr, MonadIO)
 
 type ServerApp = ServerAppT IO
 
-instance Monad m => MonadMetrics (ServerAppT m) where 
+instance Monad m => MonadMetrics (ServerAppT m) where
     getMetrics = asks Config.configMetrics
+
+instance MonadIO m => Katip (ServerAppT m) where
+    getLogEnv = asks configLogEnv
+    localLogEnv f = local (\s -> s { configLogEnv = f (configLogEnv s)})
+
+instance MonadIO m => MonadLogger (ServerAppT m) where
+    monadLoggerLog = liftKatip logMsg
+
+instance MonadIO m => MonadLogger (KatipT m) where
+    monadLoggerLog = liftKatip logMsg
